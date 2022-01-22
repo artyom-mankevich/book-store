1273d81442b882eb936f94b4228c65e9f6278879
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-01-22 23:32:03 +0300

    Removed asyncio usage

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,5 @@
-import asyncio
 import json
 
-from asgiref.sync import sync_to_async
 from django.conf.global_settings import LOGIN_URL
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -15,8 +13,8 @@ from .models import Order, OrderItem
 
 def get_cart_data(request):
     user = request.user
-    order, created = asyncio.run(create_order(user))
-    items = asyncio.run(get_all_order_items(order))
+    order, created = create_order(user)
+    items = get_all_order_items(order)
     cart_items = order.items_count
     return {'cart_items': cart_items, 'order': order, 'items': items}
 
@@ -30,7 +28,7 @@ def index(request):
     items = data['items']
 
     context = {'items': items, 'order': order, 'cart_items': cart_items}
-    return render(request, 'cart/index.html', context)
+    return render(request, 'cart/cart.html', context)
 
 
 @login_required(login_url=LOGIN_URL)
@@ -40,10 +38,10 @@ def update_item(request):
     action = data['action']
 
     user = request.user
-    product = asyncio.run(get_product(product_id))
-    order, created = asyncio.run(create_order(user=user, complete=False))
+    product = get_product(product_id)
+    order, created = create_order(user=user, complete=False)
 
-    order_item, created = asyncio.run(create_order_item(order, product))
+    order_item, created = create_order_item(order, product)
 
     if action == 'add':
         order_item.quantity += 1
@@ -64,7 +62,7 @@ def save_order(request):
     order_id = data['orderId']
     action = data['action']
 
-    order = asyncio.run(get_order(order_id))
+    order = get_order(order_id)
 
     if action == 'save':
         order.complete = True
@@ -74,26 +72,21 @@ def save_order(request):
     return HttpResponse(status=200)
 
 
-@sync_to_async
 def get_product(product_id):
     return Book.objects.get(id=product_id)
 
 
-@sync_to_async
 def create_order(user, complete=False):
     return Order.objects.get_or_create(user=user, complete=complete)
 
 
-@sync_to_async
 def create_order_item(order, product):
     return OrderItem.objects.get_or_create(order=order, product=product)
 
 
-@sync_to_async
 def get_order(pk):
     return Order.objects.get(pk=pk)
 
 
-@sync_to_async
 def get_all_order_items(order):
     return order.orderitem_set.all()
--- products/views.py
+++ products/views.py
@@ -1,6 +1,3 @@
-import asyncio
-
-from asgiref.sync import sync_to_async
 from django.views import generic
 
 from .models import Book
@@ -13,23 +10,19 @@ class ProductsView(generic.ListView):
     context_object_name = 'products'
 
     def get_queryset(self):
-        @sync_to_async
-        def get_products():
-            query_set = Book.objects.all()
+        query_set = Book.objects.all()
 
-            query_set = exact_filter(self, query_set, 'full_name', 'author')
-            query_set = exact_filter(self, query_set, 'name', 'category')
-            query_set = exact_filter(self, query_set, 'name', 'subcategory')
-            query_set = exact_filter(self, query_set, 'name', 'publisher')
+        query_set = exact_filter(self, query_set, 'full_name', 'author')
+        query_set = exact_filter(self, query_set, 'name', 'category')
+        query_set = exact_filter(self, query_set, 'name', 'subcategory')
+        query_set = exact_filter(self, query_set, 'name', 'publisher')
 
-            query_set = range_filter(self, query_set, 'price')
-            query_set = range_filter(self, query_set, 'year')
-            query_set = range_filter(self, query_set, 'pages')
+        query_set = range_filter(self, query_set, 'price')
+        query_set = range_filter(self, query_set, 'year')
+        query_set = range_filter(self, query_set, 'pages')
 
-            query_set = order_by_query(self, query_set)
-            return query_set
-
-        return asyncio.run(get_products())
+        query_set = order_by_query(self, query_set)
+        return query_set
 
 
 class ProductByIdView(generic.DetailView):
